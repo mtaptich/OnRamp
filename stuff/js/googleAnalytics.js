dc03fdd33473989cb6e04a278419505e7f4a7188
lewis <lewis@desktop.(none)>
2014-03-20 13:36:14 -0700

    a million things.

---
+++ stuff/js/googleAnalytics.js
@@ -0,0 +1,38 @@
+// (function(i, s, o, g, r, a, m) {
+//   i['GoogleAnalyticsObject'] = r;
+//   i[r] = i[r] || function() {
+//     (i[r].q = i[r].q || []).push(arguments)
+//   }, i[r].l = 1 * new Date();
+//   a = s.createElement(o),
+//   m = s.getElementsByTagName(o)[0];
+//   a.async = 1;
+//   a.src = g;
+//   m.parentNode.insertBefore(a, m)
+// })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+// ga('create', 'UA-40413119-1', 'bootply.com');
+// ga('send', 'pageview');
+// var _qevents = _qevents || [];
+
+// (function() {
+//   var elem = document.createElement('script');
+//   elem.src = (document.location.protocol == "https:" ? "https://secure" : "http://edge") + ".quantserve.com/quant.js";
+//   elem.async = true;
+//   elem.type = "text/javascript";
+//   var scpt = document.getElementsByTagName('script')[0];
+//   scpt.parentNode.insertBefore(elem, scpt);
+// })();
+
+// _qevents.push({
+//   qacct: "p-0cXb7ATGU9nz5"
+// });
+
+// (function(d, s, id) {
+//   var js, fjs = d.getElementsByTagName(s)[0],
+//     p = /^http:/.test(d.location) ? 'http' : 'https';
+//   if (!d.getElementById(id)) {
+//     js = d.createElement(s);
+//     js.id = id;
+//     js.src = p + '://platform.twitter.com/widgets.js';
+//     fjs.parentNode.insertBefore(js, fjs);
+//   }
+// }(document, 'script', 'twitter-wjs'))();
